82c3d7c1c2f22109914e11d264c78577d12ddf91
erisa <erisa.hoxha@ffuf.de>
2023-11-11 11:55:08 +0100

    start massage button

--- app/src/main/java/com/example/hackathon/MainActivity.kt
+++ app/src/main/java/com/example/hackathon/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.hackathon
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.findNavController
@@ -9,8 +10,7 @@ import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
-import android.widget.ListView
+import android.widget.*
 import com.example.hackathon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -34,6 +34,49 @@ class MainActivity : AppCompatActivity() {
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
         }
+
+        findViewById<RadioButton>(R.id.full_body).setOnCheckedChangeListener { buttonView, isChecked ->
+            Log.d("RADIO", "full_body is checked: $isChecked")
+        }
+
+        findViewById<RadioButton>(R.id.upper_back).setOnCheckedChangeListener { buttonView, isChecked ->
+            Log.d("RADIO", "upper_back is checked: $isChecked")
+        }
+
+        // when clicking the button, the request is made
+        findViewById<Button>(R.id.bStart).setOnClickListener {
+
+//            to start the massage, the three radio button groups should be checked
+//            massage type
+            var typeId = findViewById<RadioGroup>(R.id.massage_type).checkedRadioButtonId
+
+//            pace
+            var paceId = findViewById<RadioGroup>(R.id.pace_group).checkedRadioButtonId
+
+//            time
+            var timeId = findViewById<RadioGroup>(R.id.time_group).checkedRadioButtonId
+            if(typeId != -1 && paceId != -1 && timeId != -1) {
+                // find the radiobutton by returned id
+                var type: RadioButton = findViewById(typeId)
+
+                Log.d(
+                    "MASSAGE TYPE: ",
+                    "This type is selected: ${type.text}"
+                )
+                Log.d(
+                    "MASSAGE PACE: ",
+                    " pace selected: ${paceId}"
+                )
+
+                Log.d(
+                    "TIME:",
+                    "Time selected: ${timeId}"
+                )
+            }
+
+            Log.d("RADIO", "Button is clicked")
+        }
+
 
     }
 
